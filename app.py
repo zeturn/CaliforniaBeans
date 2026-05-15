905b6866bde23e7b7f5706e217846f44765925c8
Henry <zhr626@outlook.com>
2026-05-15 15:29:50 -0700

    Show California Beans test version

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+VERSION = "0.1.0"
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/plain; charset=utf-8")
         self.end_headers()
-        self.wfile.write(b"California Beans Docker test container\n")
+        self.wfile.write(f"California Beans Docker test container V{VERSION}\n".encode("utf-8"))
 
 if __name__ == "__main__":
     host = "0.0.0.0"
